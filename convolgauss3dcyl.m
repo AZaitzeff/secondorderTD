272e02595a44604e3888c55c53717559865fb33a
Alexander Zaitzeff <azaitzeff@gmail.com>
2020-02-27 15:55:25 -0500

    try

--- convolgauss3dcyl.m
+++ convolgauss3dcyl.m
@@ -13,7 +13,7 @@ for i = 1:n
         df=f/N;
         rb=0:df:f;
         tempval=(sum(rb.*(exp(-(r^2-2*r*rb+rb.^2)/(4*t)).*real(besseli(0,r*rb/(2*t),1))))-1/2*(exp(-(r^2-2*r*f+f^2)/(4*t))*real(besseli(0,r*f/(2*t),1)))*f);
-        totalz=tempval*df/(4*sqrt(pi)*(t)^(3/2))*exp(-(z-zs(i))^2/(4*t))*dz;
+        totalz=tempval*(df/(4*sqrt(pi)*(t)^(3/2))*exp(-(z-zs(i))^2/(4*t))*dz);
         if i==1 || i==n
             val=val+1/2*totalz;
         else
