6908c13f56c1daac6410b1474e4edc4e62ce6056
Alexander Zaitzeff <azaitzeff@gmail.com>
2020-02-27 15:51:58 -0500

    try

--- maindumbbell.m
+++ maindumbbell.m
@@ -4,25 +4,25 @@ test=1;
 if test==1
     double=0;
     T=.5;%time value
-    numpts=1001;
-    Ns=[512,1024,2048];
+    numpts=2001;
+    Ns=[4096,8192,8192*2];
     testdumbbell;
 elseif test==2
     double=1;
     T=.5;%time value
-    numpts=1001;
-    Ns=[16,32,64,128];
+    numpts=2001;
+    Ns=[64,128,256,512];
     testdumbbell;
 elseif test==3
     double=0;
     T=.75;%time value
-    numpts=1001;
-    Ns=[4096,8192];
+    numpts=2001;
+    Ns=[4096,8192,8192*2];
     testdumbbell;
 else
     double=1;
     T=.75;%time value
-    numpts=1001;
+    numpts=2001;
     Ns=[64,128,256,512];
     testdumbbell;
 end
