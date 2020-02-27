500057b227c29c684d828f84aaad0f2783d529b9
Alexander Zaitzeff <azaitzeff@gmail.com>
2020-02-27 14:12:02 -0500

    try

--- createerrortableparamcyl.m
+++ createerrortableparamcyl.m
@@ -1,19 +1,20 @@
 
-T=.75;
-numpts=1001;
-N=256;
+T=.5;
+numpts=501;
+N=8192;
 vars=load(['results/db' num2str(T*100) 'np' num2str(numpts) 'N' num2str(N) '.mat']);
 truer=vars.r;
 truez=vars.z;
 dz=truez(2)-truez(1);
-
-nts=[8,16,32,64];
+plot(truez,truer,'k');hold on
+nts=[64,128,256,512];
 n=numel(nts);
 error1=zeros(2,n);
 for numt=1:n
     N=nts(numt);
     load(['results/dbdouble' num2str(T*100) 'np' num2str(numpts) 'N' num2str(N) '.mat']);
     error1(1,numt)=sqrt(dz*sum((r-truer).^2));
+    plot(z,r);
 end
 
 for i=2:n
--- maindumbbell.m
+++ maindumbbell.m
@@ -4,14 +4,14 @@ test=1;
 if test==1
     double=0;
     T=.5;%time value
-    numpts=501;
-    Ns=[4096,8192];
+    numpts=1001;
+    Ns=[1024,2048,4096];
     testdumbbell;
 elseif test==2
     double=1;
     T=.5;%time value
-    numpts=501;
-    Ns=[64,128,256,512];
+    numpts=1001;
+    Ns=[32,64,128,256];
     testdumbbell;
 elseif test==3
     double=0;
